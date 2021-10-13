c364bb6a49462720ab13ed4986a61c0469be0f32
Maxime Singhathip <maximesingh06@gmail.com>
2021-10-13 16:45:26 +0200

    envoie dépot distant

---
+++ modal.js
@@ -0,0 +1,43 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+
+//  Fermer la Modale // 
+
+
+const closeBtn = document.querySelector(".close");
+
+
+closeBtn.addEventListener('click',function(){
+
+  modalbg.style.display="none";
+
+}
+)
+
+
+
+// Implémenter entrées du formulaire//
+
+console.log(testjjj);
+
+console.log(modif);
